3ead8dcdc3c6162c5f149ef994123761dddab6b5
Marcel Peschka <marcelpeschka@peschi.info>
2025-02-26 11:26:10 +0100

    changed wled to b3

--- darts-hub/control/ProfileManager.cs
+++ darts-hub/control/ProfileManager.cs
@@ -498,7 +498,7 @@ namespace darts_hub.control
                 MacX64 = "https://github.com/Peschi90/darts-wled/releases/download/v***VERSION***/darts-wled-mac",
                 MacArm64 = "https://github.com/Peschi90/darts-wled/releases/download/v***VERSION***/darts-wled-mac"
             };
-            dartsWledDownloadUrl = dartsWledDownloadMap.GetDownloadUrlByOs("2.0.0.b2");
+            dartsWledDownloadUrl = dartsWledDownloadMap.GetDownloadUrlByOs("2.0.0.b3");
 
 
             var dartsPixelitDownloadMap = new DownloadMap
